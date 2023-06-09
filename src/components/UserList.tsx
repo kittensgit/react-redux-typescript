be7fd9785959a888f84682ff608d9f5bbae06020
Nika <nikaonime@gmail.com>
2023-06-09 11:30:44 +0300

    fetchUsers

--- src/components/UserList.tsx
+++ src/components/UserList.tsx
@@ -1,10 +1,33 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import { useTypedSelector } from '../hooks/useTypedSelector';
+import { useDispatch } from 'react-redux';
+import { fetchUsers } from '../store/action-creators/user';
+import type { } from 'redux-thunk/extend-redux';
 
 const UserList: React.FC = () => {
-    const {users, loading, error} = useTypedSelector(state => state.user)
+    const { users, loading, error } = useTypedSelector(state => state.user)
+
+    const dispatch = useDispatch()
+
+    useEffect(() => {
+        dispatch(fetchUsers())
+    }, [])
+
+    if (loading) {
+        return <h1>Loading...</h1>
+    }
+    if (error) {
+        return <h1>Error 404</h1>
+    }
+
     return (
-        <div>UserList</div>
+        <div>
+            {users.map(user => <div key={user.id}>
+                {user.name}
+            </div>
+            )}
+        </div>
+
     )
 }
 
